41bf26e3bf7ff2d136ce08cff6736b9daa29bf9d
jonbondamon <83736368+jonbondamon@users.noreply.github.com>
2022-11-04 22:56:45 -0400

    yaml file setup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import yaml
+
 def main():
-    pass
+    
+    with open("settings.yml", mode="r") as yaml_file:
+        settings_yaml = yaml.safe_load(yaml_file)
+
+    print(settings_yaml['discord_token'])
 
 if __name__ == "__main__":
     main()
